504e27a8886b566482729f2fb04e02c3a0ea93bc
Aundra Miller <miller.aundra@gmail.com>
2016-03-18 09:35:45 -0700

    adjust getUsername to successfully append to index.html

--- js/github-user.js
+++ js/github-user.js
@@ -1,9 +1,16 @@
 var apiKey = require('./../.env').apiKey;
+//var getRepositories = require('./github-repos.js').getRepositories;
 
 exports.getUsername = function(username) {
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-    //set path to return info
-    console.log(response);
+    console.log("inside getUsername");
+    $('#foundUser').text(response.login);
+    console.log(response.login);
+    $('#fullName').text(response.name);
+    console.log(response.name);
+    //run getRepositories
+    //getRepositories();
+
   }).fail(function(error){
     alert(error.responseJSON.message);
   });
