e84f7d1bb3ec46882a981fdebd289b812cb83ec3
Aundra Miller <miller.aundra@gmail.com>
2016-03-18 12:08:42 -0700

    begin adding code to retrieve more than 30 repos

--- js/github-repos.js
+++ js/github-repos.js
@@ -1,6 +1,23 @@
 var apiKey = require('./../.env').apiKey;
 
 exports.getRepositories = function(username){
+  //call the header
+  // var header = curl -I 'https://api.github.com/users/' + username + '/repos?access_token=' + apiKey
+  // $.get(curl -I 'https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(header) {
+  //   //search for first page=2
+  //   var search = 'page=';
+  //   if (header.search(search) !== -1) {
+  //     //remove the first instance of page=
+  //     var newLink = string.remove(search, '');
+  //     //search new link for index of page= for new page number
+  //     var lastPageIndex = string.search('page=');
+  //     var pageNumber = string.substr(lastPageIndex + 5, 1);
+  //     console.log(pageNumber);
+  //   }
+  // }).fail(function(error) {
+  //   alert(error.responseJSON.message);
+  // });
+
   $.get('https://api.github.com/users/' + username + '/repos?sort=created&access_token=' + apiKey).then(function(response) {
 
     $('#publicRepos').empty();
